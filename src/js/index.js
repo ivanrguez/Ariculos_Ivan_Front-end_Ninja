f4edaeabd3816f8a6c8b190ae0a46338b703111b
Ivan Rodiguez <ivanisaacperal@gmail.com>
2017-01-31 00:52:00 +0100

    segunda subida, menu resposive y linkedin

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+var $ = require('jquery');
+/*enlace linkedin*/
+function funcionLinkedin(url){
+window.open('https://es.linkedin.com/in/ivan-rodriguez-pulido','_blank');
+}
---
+++ src/js/main.js
@@ -0,0 +1,2 @@
+require('./menu-responsive');
+require('./index');
